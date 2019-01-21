394898d2a1bd8443029055410426b51d4076e8b8
unknown <ao.marine23@gmail.com>
2019-01-21 14:41:01 +0700

    Initial

--- index.php
+++ index.php
@@ -12,23 +12,30 @@ if (!is_null($events['events'])) {
 // Loop through each event 
 foreach ($events['events'] as $event) { 
 // Line API send a lot of event type, we interested in message only. 
-if ($event['type'] == 'message') { 
+if ($event['type'] == 'message' && $event['message']['type'] == 'text') { 
 // Get replyToken 
 $replyToken = $event['replyToken']; 
-switch($event['message']['type']) { 
-case 'image': 
-$messageID = $event['message']['id']; 
-$respMessage = 'Hello, your image ID is '. $messageID; 
- 
-break; 
-default: 
-$respMessage = 'Please send image only'; 
-break; 
+// Split message then keep it in database. 
+$appointments = explode(',', $event['message']['text']); 
+if(count($appointments) == 2) { 
+$host = 'ec2-174-129-223-193.compute-1.amazonaws.com'; 
+$dbname = 'd74bjtc28mea5m'; 
+$user = 'eozuwfnzmgflmu'; 156 
+
+$pass = '2340614a293db8e8a8c02753cd5932cdee45ab90bfcc19d0d306754984cbece1'; 
+$connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass); 
+$params = array( 
+'time' => $appointments[0], 
+'content' => $appointments[1], 
+); 
+$statement = $connection->prepare("INSERT INTO appointments (time, content) VALUES (:time, :content)"); 
+$result = $statement->execute($params); 
+$respMessage = 'Your appointment has saved.'; 
+}else{ 
+$respMessage = 'You can send appointment like this "12.00,House keeping." '; 
 } 
 $httpClient = new CurlHTTPClient($channel_token); 
-$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 100 
-
- 
+$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret)); 
 $textMessageBuilder = new TextMessageBuilder($respMessage); 
 $response = $bot->replyMessage($replyToken, $textMessageBuilder); 
 } 
