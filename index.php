2aab71b55cb2f3fffedb83dd22049797d10404d5
unknown <ao.marine23@gmail.com>
2019-01-21 14:18:27 +0700

    Initial

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient; 
 use \LINE\LINEBot; 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder; 
-$channel_token = '1v2OUa9tuMIiDhEg57ANbsRaBDbBGP9nlCC+Dpvt5HrsQ+LqcrImWPUBkH8re/pwqxv56d15kZeMoU/vQ0zuzPFlbhFM7AhRMZwLrSkLdcjbFurwXGOyHLt8MdgzLfAe7r0BsQV5cATlUanW3OgJewdB04t89/1O/w1cDnyilFU='; 
-$channel_secret = '9b2c7349ea939ef723a3cb453d774c86'; 
+$channel_token = 'eYl7FdkSJdfJFLklZ3+spXp9UYinc0UL9gzMeY+ljuyErQBr67nNloi2YjPMFUBRKMq+ECtQ9QXZheV/PwCyzpcjNcPnD4TYboH1hGNsIMyrE7BkgJvqENoicxdSOHAs9Tg36U40VDs85EIPNl9zGQdB04t89/1O/w1cDnyilFU='; 
+$channel_secret = '11c5937f8f7deeddd86bd9f584106a85'; 
 // Get message from Line API 
 $content = file_get_contents('php://input'); 
 $events = json_decode($content, true); 
